40753bc25dba220a8cbb01e0ed6d94a3f20c575e
Charles <chabermehl@unm.edu>
2018-10-25 09:51:06 -0600

    added comment

--- src/Fire_Agents/Main.java
+++ src/Fire_Agents/Main.java
@@ -1,4 +1,8 @@
 package Fire_Agents;
 
+/*
+just gonna use this file to instantiate our gui so we can have multiple instances
+also could be used to test if everything is working.
+ */
 public class Main {
 }
