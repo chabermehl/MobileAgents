2f46608fa835377b6829d3230350420c94086f8e
Charles <chabermehl@unm.edu>
2018-10-25 10:49:13 -0600

    added comments

--- src/Fire_Agents/Node.java
+++ src/Fire_Agents/Node.java
@@ -3,6 +3,7 @@ package Fire_Agents;
 import java.util.LinkedList;
 
 public class Node {
+    //initialized variables for constructor
     private String name;
     private int x;
     private int y;
@@ -10,6 +11,15 @@ public class Node {
     private LinkedList<String> neighbors;
     private boolean hasAgent;
 
+    /**
+     * creates a node with a position, initialized states and that nodes neighbors
+     * @param name name of the node(A, B, C, etc.)
+     * @param x x-coordinate for node position
+     * @param y y-coordinate for node position
+     * @param state alive, hot, dead
+     * @param neighbors list of neighboring nodes
+     * @param hasAgent boolean to say if there is an agent on the node
+     */
     public Node(String name, int x, int y, String state, LinkedList<String> neighbors, boolean hasAgent) {
         this.name = name;
         this.x = x;
