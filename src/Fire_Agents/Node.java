48d380edafee4e8ee5d64b98678e76596e1e2ca4
Charles <chabermehl@unm.edu>
2018-10-25 10:43:13 -0600

    added node constructor

--- src/Fire_Agents/Node.java
+++ src/Fire_Agents/Node.java
@@ -1,4 +1,21 @@
 package Fire_Agents;
 
+import java.util.LinkedList;
+
 public class Node {
+    private String name;
+    private int x;
+    private int y;
+    private String state;
+    private LinkedList<String> neighbors;
+    private boolean hasAgent;
+
+    public Node(String name, int x, int y, String state, LinkedList<String> neighbors, boolean hasAgent) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.state = state;
+        this.neighbors = neighbors;
+        this.hasAgent = hasAgent;
+    }
 }
