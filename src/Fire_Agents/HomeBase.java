fe3dafe528a30303d91f4d61b17d68109283f8e0
Charles <chabermehl@unm.edu>
2018-10-25 10:53:49 -0600

    added comment

--- src/Fire_Agents/HomeBase.java
+++ src/Fire_Agents/HomeBase.java
@@ -1,4 +1,9 @@
 package Fire_Agents;
 
+/*
+use this class to initialize agents and give them somewhere to report back to
+possibly make a popup on the gui when a node is reported on fire?
+ */
+
 public class HomeBase {
 }
