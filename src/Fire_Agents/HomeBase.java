90a062790cca0ce8d38e6e88b9d11ca0e700a8f5
Charles <chabermehl@unm.edu>
2018-10-25 11:07:56 -0600

    updated comments

--- src/Fire_Agents/HomeBase.java
+++ src/Fire_Agents/HomeBase.java
@@ -3,7 +3,11 @@ package Fire_Agents;
 /*
 use this class to initialize agents and give them somewhere to report back to
 possibly make a popup on the gui when a node is reported on fire?
+
+or are we better off creating a home base node?
+
+should we make a node interface instead?
  */
 
-public class HomeBase {
+public class HomeBase extends Node {
 }
