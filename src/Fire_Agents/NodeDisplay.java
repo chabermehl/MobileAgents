0807f1d3188ec9ae5bc05a8c70f6ac3a18261776
Charles <chabermehl@unm.edu>
2018-10-25 19:15:54 -0600

    added start method

--- src/Fire_Agents/NodeDisplay.java
+++ src/Fire_Agents/NodeDisplay.java
@@ -1,7 +1,15 @@
 package Fire_Agents;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /*
 GUI code goes here
  */
-public class NodeDisplay {
+public class NodeDisplay extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
 }
