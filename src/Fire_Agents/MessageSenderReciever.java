8ef482a39f32945596c27895d4a0494b6852979f
Alex Booher <alex.booher@yahoo.com>
2018-10-30 17:58:32 -0600

    Removed Message Interface (Replaced with class)

--- src/Fire_Agents/MessageSenderReciever.java
+++
@@ -1,9 +0,0 @@
-package Fire_Agents;
-
-public interface MessageSenderReciever {
-
-    void processMessage(Message message);
-    void sendMessage(Message message, MessageSenderReciever receiver);
-    void recieveMessage(Message message);
-
-}
