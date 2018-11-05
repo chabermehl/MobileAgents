371ca0b81c48b9d9625f96c2a3fb8ca425c2a8fd
Charles <chabermehl@unm.edu>
2018-11-04 19:35:09 -0700

    added file reader code

--- src/Fire_Agents/TextFileReader.java
+++ src/Fire_Agents/TextFileReader.java
@@ -1,4 +1,27 @@
 package Fire_Agents;
 
+import java.io.*;
+import java.util.LinkedList;
+
 public class TextFileReader {
+    LinkedList<String> FileToList(String fileName){
+        LinkedList<String> fileList = new LinkedList<>();
+        String line;
+        try{
+            InputStream file = getClass().getClassLoader().getResourceAsStream(fileName);
+            InputStreamReader fileReader = new InputStreamReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while ((line = bufferedReader.readLine()) != null){
+                fileList.add(line);
+            }
+            bufferedReader.close();
+        }
+        catch (FileNotFoundException ex){
+            System.out.println("No file found with name " + fileName);
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
+        return fileList;
+    }
 }
