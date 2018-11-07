716031185b0f42918704317c09d2b5e3640ea796
Charles <chabermehl@unm.edu>
2018-11-06 19:17:23 -0700

    added xy coord setters

--- src/Fire_Agents/Edge.java
+++ src/Fire_Agents/Edge.java
@@ -1,4 +1,26 @@
 package Fire_Agents;
 
 public class Edge {
+
+    private int startX = 0;
+    private int startY = 0;
+    private int endX = 0;
+    private int endY = 0;
+
+    public Edge(int startX, int startY, int endX, int endY) {
+        this.startX = startX;
+        this.startY = startY;
+        this.endX = endX;
+        this.endY = endY;
+    }
+
+    public void setStartXY(int x, int y) {
+        this.startX = x;
+        this.startY = y;
+    }
+
+    public void setEndXY(int x, int y) {
+        this.endX = x;
+        this.endY = y;
+    }
 }
