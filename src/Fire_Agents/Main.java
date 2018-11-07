d6ede4ca84df39be8a9e029d411feacf92f850e7
Charles <chabermehl@unm.edu>
2018-11-06 19:23:55 -0700

    most effecient way to update my repo

--- src/Fire_Agents/Main.java
+++ src/Fire_Agents/Main.java
@@ -1,12 +1,46 @@
 package Fire_Agents;
 
-import javafx.application.Application;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 /*
 just gonna use this file to instantiate our gui so we can have multiple instances
 also could be used to test if everything is working.
  */
+
 public class Main {
 
+    public static void main(String[] args) {
 
+        // Create test nodes
+        HomeBase homeBase = new HomeBase();
+        Node node1 = new Node(2,0, new LinkedList<>());
+        Node node2 = new Node(4,0, new LinkedList<>());
+        Node node3 = new Node (6,0, new LinkedList<>());
+
+        // Add neighbors
+        homeBase.addNeighbor(node1);
+        node1.addNeighbor(node2);
+        node2.addNeighbor(node3);
+
+        // Create threads
+        Thread homeBaseThread = new Thread(homeBase);
+        Thread nodeThread1 = new Thread(node1);
+        Thread nodeThread2 = new Thread(node2);
+        Thread nodeThread3 = new Thread(node3);
+
+        // Start node threads
+        homeBaseThread.start();
+        nodeThread1.start();
+        nodeThread2.start();
+        nodeThread3.start();
+
+        // Set the last node on fire
+        try {
+            node3.setState(Node.State.FIRE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
