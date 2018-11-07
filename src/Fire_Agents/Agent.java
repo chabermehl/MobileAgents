172f6efcc199003814d59a0c1ae97baeb69602dd
Charles <chabermehl@unm.edu>
2018-11-06 19:23:28 -0700

    most effecient way to update my repo

--- src/Fire_Agents/Agent.java
+++ src/Fire_Agents/Agent.java
@@ -1,4 +1,41 @@
 package Fire_Agents;
 
-public class Agent {
+public class Agent extends MessageProcessor implements Runnable {
+
+    private String name;
+    private Node currentNode;
+    private static int agentCount = 0;
+
+    public Agent(HomeBase base) {
+        agentCount++;
+        name = "Agent_" + agentCount;
+        currentNode = base;
+    }
+
+    public Node getCurrentNode()
+    {
+        return currentNode;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            try {
+                processMessage(pollNextMessage());
+            } catch (InterruptedException e) {
+                System.out.println(this.name + "'s thread was interrupted.");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void processMessage(Message message) {
+
+    }
+
 }
--- src/Fire_Agents/HomeBase.java
+++ src/Fire_Agents/HomeBase.java
@@ -9,9 +9,35 @@ or are we better off creating a home base node?
 should we make a node interface instead?
  */
 
+import java.util.LinkedList;
+
 public class HomeBase extends Node {
+
+    private LinkedList<Agent> agentsCreated = new LinkedList<>();
 
     public HomeBase() {
         super();
     }
+
+    private void createAgent()
+    {
+        agentsCreated.add(new Agent(this));
+    }
+
+    @Override
+    public void processMessage(Message message)
+    {
+        switch(message.getMessageType())
+        {
+            case CREATE_AGENT:
+                // Create an agent and send them toward the next set of yellow nodes
+                createAgent();
+                Thread agentThread = new Thread(agentsCreated.getLast());
+                agentThread.start();
+                break;
+            case NODE_DIED:
+                System.out.println("a node died. gonna die soon");
+        }
+    }
+
 }
