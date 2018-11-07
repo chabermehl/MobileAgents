b0e10a6e707afd6bc5bbdada9b551dff08b5c36a
Charles <chabermehl@unm.edu>
2018-11-06 23:16:17 -0700

    added javadoc comment
    added todo

--- src/Fire_Agents/InitializeNodes.java
+++ src/Fire_Agents/InitializeNodes.java
@@ -10,7 +10,12 @@ public class InitializeNodes {
     public LinkedList<Edge> edges;
     public boolean validGraph;
 
-    public boolean initializeGraph(String mapFile) {
+    /**
+     * initializes all the nodes and edges for the gui
+     * @param mapFile config file that is passed in
+     */
+    //TODO: fix unhandled exceptions
+    public void initializeGraph(String mapFile) {
         validGraph = true;
         nodes = new LinkedList<>();
         edges = new LinkedList<>();
@@ -18,38 +23,35 @@ public class InitializeNodes {
         largestRow = 0;
         TextFileReader textReader = new TextFileReader();
         LinkedList<String> fileLines = textReader.FileToList(mapFile);
-        for(String line : fileLines) {
+        for (String line : fileLines) {
             String[] lines = line.split(" ");
-            for(int i = 0; i < lines.length; i++) {
-                if("station".equals(lines[0])) {
+            for (int i = 0; i < lines.length; i++) {
+                if ("station".equals(lines[0])) {
                     HomeBase station = new HomeBase();
                     station.setX(Integer.parseInt(lines[1]));
                     station.setY(Integer.parseInt(lines[2]));
                     nodes.add(station);
-                }
-                else if("node".equals(lines[0])) {
+                } else if ("node".equals(lines[0])) {
                     Node node = new Node(Integer.parseInt(lines[1]), Integer.parseInt(lines[2]), new LinkedList<>());
                     nodes.add(node);
-                }
-                else if("edge".equals(lines[0])) {
+                } else if ("edge".equals(lines[0])) {
                     Edge edge = new Edge(Integer.parseInt(lines[1]), Integer.parseInt(lines[2]), Integer.parseInt(lines[3]), Integer.parseInt(lines[4]));
                     edges.add(edge);
-                }
-                else if("fire".equals(lines[0])) {
-                    if(nodes.isEmpty()) {
+                } else if ("fire".equals(lines[0])) {
+                    if (nodes.isEmpty()) {
                         Node node = new Node(Integer.parseInt(lines[1]), Integer.parseInt(lines[2]), new LinkedList<>());
                         //need to handle the exception in setState
                         node.setState(Node.State.FIRE);
                         nodes.add(node);
                     } else {
                         boolean notFound = true;
-                        for(Node tempNode : nodes) {
-                            if((tempNode.getX() == Integer.parseInt(lines[1])) && (tempNode.getY() == Integer.parseInt(lines[2])) && (notFound)) {
+                        for (Node tempNode : nodes) {
+                            if ((tempNode.getX() == Integer.parseInt(lines[1])) && (tempNode.getY() == Integer.parseInt(lines[2])) && (notFound)) {
                                 tempNode.setState(Node.State.FIRE);
                                 notFound = false;
                             }
                         }
-                        if(!notFound) {
+                        if (!notFound) {
                             Node node = new Node(Integer.parseInt(lines[1]), Integer.parseInt(lines[2]), new LinkedList<>());
                             node.setState(Node.State.FIRE);
                             nodes.add(node);
@@ -60,6 +62,5 @@ public class InitializeNodes {
                 }
             }
         }
-        return validGraph;
     }
 }
