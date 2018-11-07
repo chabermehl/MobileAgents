dc3337b6e933f0fb34d078baacc44f88be2c4dfd
Charles <chabermehl@unm.edu>
2018-11-06 19:43:58 -0700

    added globals

--- src/Fire_Agents/InitializeNodes.java
+++ src/Fire_Agents/InitializeNodes.java
@@ -1,6 +1,14 @@
 package Fire_Agents;
 
+import java.util.LinkedList;
+
 public class InitializeNodes {
+
+    int largestRow;
+    int largetCol;
+    LinkedList<Node> nodes;
+    LinkedList<Edge> edges;
+    boolean validGraph;
 
     public boolean initializeGraph(String mapFile) {
 
