c25ebc4b77e0d8eb62810e695b6f2f22b41e57a2
Charles <chabermehl@unm.edu>
2018-11-06 20:48:19 -0700

    wrote main logic for parsing nodes

--- src/Fire_Agents/InitializeNodes.java
+++ src/Fire_Agents/InitializeNodes.java
@@ -4,11 +4,11 @@ import java.util.LinkedList;
 
 public class InitializeNodes {
 
-    int largestRow;
-    int largetCol;
-    LinkedList<Node> nodes;
-    LinkedList<Edge> edges;
-    boolean validGraph;
+    private int largestRow;
+    private int largetCol;
+    private LinkedList<Node> nodes;
+    private LinkedList<Edge> edges;
+    private boolean validGraph;
 
     public boolean initializeGraph(String mapFile) {
         validGraph = true;
@@ -18,12 +18,23 @@ public class InitializeNodes {
         largestRow = 0;
         TextFileReader textReader = new TextFileReader();
         LinkedList<String> fileLines = textReader.FileToList(mapFile);
-        String[] graphStrings = new String[fileLines.size()];
-        graphStrings = fileLines.toArray(graphStrings);
+        for(String line : fileLines) {
+            String[] lines = line.split(" ");
+            for(int i = 0; i < lines.length; i++) {
+                if("station".equals(lines[0])) {
+                    HomeBase station = new HomeBase();
+                }
+                else if("node".equals(lines[0])) {
 
-        for(String line : graphStrings) {
-            for(int i = 0; i < line.length(); i++) {
+                }
+                else if("edge".equals(lines[0])) {
 
+                }
+                else if("fire".equals(lines[0])) {
+
+                } else {
+                    System.out.println("Invalid Config File\n");
+                }
             }
         }
         return validGraph;
