53096a5d8f24e72db5f2ced519836223d9782576
Charles <chabermehl@unm.edu>
2018-11-08 11:14:12 -0700

    added functionality to start function created startScene method

--- src/Fire_Agents/NodeDisplay.java
+++ src/Fire_Agents/NodeDisplay.java
@@ -7,12 +7,21 @@ import javafx.stage.Stage;
 GUI code goes here
  */
 public class NodeDisplay extends Application {
+
+    private Stage window;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
+        window = primaryStage;
+        window.setTitle("Mobile Agents");
+        startScene();
+    }
+
+    public void startScene() {
 
     }
 }
