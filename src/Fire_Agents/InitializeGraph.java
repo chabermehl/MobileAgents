2898b91fd0101f69ba953d55f0dc73a34cdcfee7
Charles <chabermehl@unm.edu>
2018-11-08 10:27:23 -0700

    wrote method that initilizes graph so that it can be threaded

--- src/Fire_Agents/InitializeGraph.java
+++ src/Fire_Agents/InitializeGraph.java
@@ -1,4 +1,22 @@
 package Fire_Agents;
 
+import java.util.LinkedList;
+
 public class InitializeGraph {
+
+    public LinkedList<Node> nodes;
+    public LinkedList<Edge> edges;
+    public int maxCol;
+    public int maxRow;
+
+    public void graphInitialization(String fileName) {
+        InitializeNodes initializeNodes = new InitializeNodes();
+        initializeNodes.initializeGraph(fileName);
+        initializeNodes.buildNeighborLists();
+        maxCol = initializeNodes.getLargestCol();
+        maxRow = initializeNodes.getLargestRow();
+        nodes = initializeNodes.getNodes();
+        edges = initializeNodes.getEdges();
+    }
+
 }
