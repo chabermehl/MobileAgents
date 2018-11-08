59cc8922a4fdc9bc97eb739d81b0e39202c22873
Charles <chabermehl@unm.edu>
2018-11-08 10:43:08 -0700

    added comments

--- src/Fire_Agents/InitializeGraph.java
+++ src/Fire_Agents/InitializeGraph.java
@@ -9,6 +9,10 @@ public class InitializeGraph {
     public int maxCol;
     public int maxRow;
 
+    /**
+     * initializes graph based on config file that is passed in
+     * @param fileName text file with node/edge definitions
+     */
     public void graphInitialization(String fileName) {
         InitializeNodes initializeNodes = new InitializeNodes();
         initializeNodes.initializeGraph(fileName);
@@ -19,6 +23,9 @@ public class InitializeGraph {
         edges = initializeNodes.getEdges();
     }
 
+    /**
+     * loops through the nodes list and creates and starts the threads
+     */
     public void startThreads() {
         for(Node tempNode : nodes) {
             Thread threadedNode = new Thread(tempNode);
