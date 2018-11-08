d29955963456c2ad1961f00a53d063555f15458c
Charles <chabermehl@unm.edu>
2018-11-08 12:11:46 -0700

    created graph loader method

--- src/Fire_Agents/GraphLoader.java
+++ src/Fire_Agents/GraphLoader.java
@@ -1,4 +1,14 @@
 package Fire_Agents;
 
+import java.io.File;
+import java.util.LinkedList;
+
 public class GraphLoader {
+    public LinkedList<String> graphList(final File folder) {
+        LinkedList<String> list  = new LinkedList<>();
+        //add new config files here
+        //list.add(<config file>
+        list.add("default");
+        return list;
+    }
 }
